5f57d16b1d57044f1297f1f3aa4ce05f0cb90bd9
Ryan <rmfarrell@gmail.com>
2018-05-23 18:38:19 -0400

    header

--- src/assets/scripts/layout/theme.js
+++ src/assets/scripts/layout/theme.js
@@ -8,47 +8,71 @@ import 'lazysizes/plugins/respimg/ls.respimg';
 import '../../styles/theme.scss';
 import '../../styles/theme.scss.liquid';
 
-import $ from 'jquery';
-import { pageLinkFocus } from '@shopify/theme-a11y';
 import { cookiesEnabled } from '@shopify/theme-cart';
 import { wrapTable, wrapIframe } from '@shopify/theme-rte';
 
 window.slate = window.slate || {};
 window.theme = window.theme || {};
 
-$(document).ready(() => {
-  // Common a11y fixes
-  if (window.location.hash !== '#') {
-    pageLinkFocus($(window.location.hash));
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+// toggle header
+const $globalHeader = $('#global-header')
+const $navToggle = $globalHeader.querySelector('.hamburger')
+const $nav = $globalHeader.querySelector('nav')
+
+$navToggle.addEventListener('click', () => {
+  $navToggle.classList.toggle('is-active')
+  $nav.classList.toggle('is-active')
+})
+
+// open parent el if data-open-parent toggler present
+const $$parentTogglers = $$('[data-open-parent]')
+each($$parentTogglers, (el) => {
+  el.addEventListener('click', () => el.parentElement.classList.toggle('open'))
+})
+
+// set 'supports-no-cookies' / 'supports-cookies' class
+if (cookiesEnabled()) {
+  document.documentElement.className = document.documentElement.className.replace(
+    'supports-no-cookies',
+    'supports-cookies',
+  );
+}
+
+// $(document).ready(() => {
+
+// // Target tables to make them scrollable
+// const tableSelectors = '.rte table';
+
+// wrapTable({
+//   $tables: $(tableSelectors),
+//   tableWrapperClass: 'rte__table-wrapper',
+// });
+
+// // Target iframes to make them responsive
+// const iframeSelectors =
+//   '.rte iframe[src*="youtube.com/embed"],' +
+//   '.rte iframe[src*="player.vimeo"]';
+
+// wrapIframe({
+//   $iframes: $(iframeSelectors),
+//   iframeWrapperClass: 'rte__video-wrapper',
+// });
+
+// Apply a specific class to the html element for browser support of cookies.
+// });
+
+// -- Utilities
+
+/**
+ * Safely iterate through elements
+ * @param {HTMLNodeList} els
+ * @param {Function} cb callback passes current element and index as args
+ */
+function each(els, cb = function () { }) {
+  for (let x = 0; x < els.length; x++) {
+    cb(els[x], x)
   }
-
-  $('.in-page-link').on('click', (evt) => {
-    pageLinkFocus($(evt.currentTarget.hash));
-  });
-
-  // Target tables to make them scrollable
-  const tableSelectors = '.rte table';
-
-  wrapTable({
-    $tables: $(tableSelectors),
-    tableWrapperClass: 'rte__table-wrapper',
-  });
-
-  // Target iframes to make them responsive
-  const iframeSelectors =
-    '.rte iframe[src*="youtube.com/embed"],' +
-    '.rte iframe[src*="player.vimeo"]';
-
-  wrapIframe({
-    $iframes: $(iframeSelectors),
-    iframeWrapperClass: 'rte__video-wrapper',
-  });
-
-  // Apply a specific class to the html element for browser support of cookies.
-  if (cookiesEnabled()) {
-    document.documentElement.className = document.documentElement.className.replace(
-      'supports-no-cookies',
-      'supports-cookies',
-    );
-  }
-});
+}
